7747a19567518fe6055fb215cb12c04ffddce05c
SirSanewa <lukaszsanewski@gmail.com>
2021-05-27 18:01:06 +0200

    zajecia 01

---
+++ zajęcia01/if_bonus1a.py
@@ -0,0 +1,8 @@
+liczba = int(input("Wpisz liczbę większą od 0: "))
+
+if liczba > 0:
+    wynik = liczba
+else:
+    wynik = 0
+
+print(wynik)
---
+++ zajęcia01/if_bonus1b.py
@@ -0,0 +1,6 @@
+liczba = int(input("Wpisz liczbę większą od 0: "))
+
+wynik = liczba if liczba > 0 else 0
+# zwróć `liczba` jeśli liczba jest większa od 0, w przeciwnyw wypadku zwróć 0
+
+print(wynik)
