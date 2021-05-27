64af7891f0b1944364ebb43cf00c7e350e7cd81e
SirSanewa <lukaszsanewski@gmail.com>
2021-05-27 17:43:15 +0200

    zajecia 01

---
+++ zajęcia01/if7.py
@@ -0,0 +1,9 @@
+liczba = int(input("Podaj liczbę całkowitą: "))
+
+if liczba > 0 and liczba < 100:
+    print('Liczba jest z zakresu (0, 100)')
+
+# wersja druga
+
+if 0 < liczba < 100:
+    print('Liczba jest z zakresu (0, 100)')
