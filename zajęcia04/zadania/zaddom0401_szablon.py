49fa49e02b16eb79659861104b100cc2c58f5abe
SirSanewa <lukaszsanewski@gmail.com>
2021-06-15 17:41:40 +0200

    zajecia 04

---
+++ zajęcia04/zadania/zaddom0401_szablon.py
@@ -0,0 +1,9 @@
+# imię i nazwisko
+
+osoby = [{'id': 0, 'imie': 'Paulina', 'nazwisko': 'Sobczak', 'aktywna': True},
+         {'id': 5, 'imie': 'Henryk', 'nazwisko': 'Bąk', 'aktywna': False},
+         {'id': 11, 'imie': 'Kazimierz', 'nazwisko': 'Górski', 'aktywna': True},
+         {'id': 3, 'imie': 'Irena', 'nazwisko': 'Wójcik', 'aktywna': True},
+         {'id': 30, 'imie': 'Władysław', 'nazwisko': 'Piotrowski', 'aktywna': False}]
+
+# tutaj wpisz swój kod
