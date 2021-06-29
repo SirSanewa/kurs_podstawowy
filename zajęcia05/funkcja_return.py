19320a8c49da57eeceadc7a86ff2b9028bed07de
SirSanewa <lukaszsanewski@gmail.com>
2021-06-29 17:59:50 +0200

    zajecia 05

---
+++ zajęcia05/funkcja_return.py
@@ -0,0 +1,14 @@
+def dodaj_trzy(liczba):
+    wynik = liczba + 3
+    return wynik
+
+
+def dodaj_dwa(liczba):
+    wynik = liczba + 2
+    print(wynik)
+
+a = dodaj_trzy(2)
+b = dodaj_dwa(2)
+
+print(a)
+print(b)
