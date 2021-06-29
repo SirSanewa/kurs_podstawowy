2f67443dc692984ff55376701c2f20eb68f2e33f
SirSanewa <lukaszsanewski@gmail.com>
2021-06-29 17:24:53 +0200

    zajecia 04 - praca domowa

---
+++ zajęcia05/funkcje2_4.py
@@ -0,0 +1,10 @@
+# funkcja, która nie przyjmuje parametrów ale zwraca wartość
+
+
+def shrug():
+    return "¯\_(ツ)_/¯"
+
+
+komunikat = f'Co zrobić, taką mamy funkcję {shrug()}'
+
+print(komunikat)
